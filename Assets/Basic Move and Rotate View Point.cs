9921c252c6d7c91ac2fded81d188dd09cc2d9b0d
Barbarrosa <barbarlosa@gmail.com>
2023-09-24 19:42:46 -0700

    9/24 Week4 Assignment

---
+++ Assets/Basic Move and Rotate View Point.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasicMoveandRotateViewPoint : MonoBehaviour
+{
+
+    public Vector3 moveFront = new Vector3(0f, 0f,z:1f);
+    public Vector3 moveBack = new Vector3(0f, 0f, z:-1f);
+    public Vector3 rotateLeft = new Vector3(0f, y: -1f, 0f);
+    public Vector3 rotateRight = new Vector3(0f, y: 1f, 0f);
+
+    public Keyboard keyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = Keyboard.current;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keyboard.wKey.isPressed)
+        {
+            transform.Translate(moveFront);
+        }
+        if (keyboard.sKey.isPressed)
+        {
+            transform.Translate(moveBack);
+        }
+        if (keyboard.dKey.isPressed)
+        {
+            transform.Rotate(rotateRight);
+        }
+        if (keyboard.aKey.isPressed)
+        {
+            transform.Rotate(rotateLeft);
+        }
+    }
+}
+
